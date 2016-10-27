ddc6c999c201c5b391add9eb7ea39e200fd205d6
zshapiro <zack@nextdoor.com>
2016-10-26 17:07:45 -0700

    Only deactivate dags that are inside the schedulers dag folder.

--- airflow/models.py
+++ airflow/models.py
@@ -414,11 +414,12 @@ class DagBag(LoggingMixin):
             'dagbag_import_errors', len(self.import_errors), 1)
 
     def deactivate_inactive_dags(self):
-        active_dag_ids = [dag.dag_id for dag in list(self.dags.values()) if dag.full_filepath.startswith(self.dag_folder)]
+        active_dag_ids = [dag.dag_id for dag in list(self.dags.values())]
         session = settings.Session()
         for dag in session.query(
                 DagModel).filter(~DagModel.dag_id.in_(active_dag_ids)).all():
-            dag.is_active = False
+            if dag.fileloc.startswith(self.dag_folder):
+                dag.is_active = False
             session.merge(dag)
         session.commit()
         session.close()
