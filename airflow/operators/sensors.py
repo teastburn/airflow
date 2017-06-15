fe25e65bef24a36c234e7019c691f65dda568c44
Jay Thomas <jay.w.thomas@gmail.com>
2017-06-15 09:54:06 -0700

    DATA-1160 -- external task dependencies

--- airflow/operators/sensors.py
+++ airflow/operators/sensors.py
@@ -478,6 +478,68 @@ class WebHdfsSensor(BaseSensorOperator):
         return c.check_for_path(hdfs_path=self.filepath)
 
 
+class ExternalTaskConsecutiveRunSensor(BaseSensorOperator):
+    """
+    Waits for a task to complete in a different DAG
+
+    :param external_dag_id: The dag_id that contains the task you want to
+        wait for
+    :type external_dag_id: string
+    :param external_task_id: The task_id that contains the task you want to
+        wait for
+    :type external_task_id: string
+    :param allowed_states: list of allowed states, default is ``['success']``
+    :type allowed_states: list
+    :param lookback_delta: how far to look back. e.g., to check
+        for a day's worth of runs, use timedelta(days=1)
+    :type lookback_delta: datetime.timedelta
+    :param expected runs: how many runs we expected to see within the
+        lookback window
+    :type expected_runs: int
+    """
+
+    @apply_defaults
+    def __init__(
+            self,
+            external_dag_id,
+            external_task_id,
+            allowed_states=None,
+            lookback_delta=None,
+            expected_runs=None,
+            *args, **kwargs):
+        super(ExternalTaskConsecutiveRunSensor, self).__init__(*args, **kwargs)
+        self.allowed_states = allowed_states or [State.SUCCESS]
+        self.lookback_delta = lookback_delta
+        self.expected_runs = expected_runs
+        self.external_dag_id = external_dag_id
+        self.external_task_id = external_task_id
+
+    def poke(self, context):
+        start_dttm = context['execution_date'] - self.lookback_delta
+        end_dttm = context['execution_date']
+
+        logging.info(
+            'Poking for '
+            '{self.external_dag_id}.'
+            '{self.external_task_id} in range '
+            '{start_dttm} to {end_dttm} -- '
+            'Expecting {self.expected_runs} runs ...'.format(**locals()))
+        TI = TaskInstance
+
+        session = settings.Session()
+        count = session.query(TI).filter(
+            TI.dag_id == self.external_dag_id,
+            TI.task_id == self.external_task_id,
+            TI.state.in_(self.allowed_states),
+            TI.execution_date >= start_dttm,
+            TI.execution_date < end_dttm
+        ).count()
+        logging.info('Found {count} runs'.format(**locals()))
+        session.commit()
+        session.close()
+        return count == self.expected_runs
+
+
 class S3KeySensor(BaseSensorOperator):
     """
     Waits for a key (a file-like instance on S3) to be present in a S3 bucket.
