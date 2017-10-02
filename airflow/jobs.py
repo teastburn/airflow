d97a6d7666167582da81f8e07f9ab6410b5c4e4f
Charlie Jones <cjones73@gmail.com>
2017-10-02 13:51:40 -0700

    Cherry-pick https://github.com/apache/airflow/commit/ebc02fb53e0faa59cb2dcbc1db13adc58a88646c

--- airflow/jobs.py
+++ airflow/jobs.py
@@ -649,7 +649,7 @@ class SchedulerJob(BaseJob):
                 notification_sent = True
             else:
                 email_content = """\
-                Here's a list of tasks thas missed their SLAs:
+                Here's a list of tasks that missed their SLAs:
                 <pre><code>{task_list}\n<code></pre>
                 Blocking tasks:
                 <pre><code>{blocking_task_list}\n{bug}<code></pre>
